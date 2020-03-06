2778bc5edf130a77137b7d45666670a2d5dabd01
Sajee <sajeebilla@gmail.com>
2020-03-06 20:48:02 +0530

    write file system

--- test.js
+++ test.js
@@ -1,11 +1,21 @@
 const fs = require('fs');
 
-fs.writeFile("/tmp/test", "Hey there!", function(err) {
-    if(err) {
-        return console.log(err);
+let test = {
+    createCustomer: function (path) {
+        test(path);
+
     }
-    console.log("The file was saved!");
-});
+};
+function test(path){
+    console.log(path)
+    fs.writeFile("/tmp/test", "Hey there!", function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    });
 
 // Or
-fs.writeFileSync('/tmp/test-sync');
+    fs.writeFileSync('/tmp/test-sync');
+}
+module.exports = test;
